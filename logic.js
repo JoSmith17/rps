77f0e8a43514271d10092e3c2cb9da6b33def2c0
Jordan Smith <jordansmith3326@yahoo.com>
2017-12-21 15:17:35 -0700

    files updated

--- logic.js
+++ logic.js
@@ -1,9 +1,41 @@
+var choices = ["rock", "paper", "scissors"];
+
 function computerMove() {
-  return 'rock'
+  var decision = choices[Math.floor(Math.random() * choices.length)];
+  return decision;
 }
 
 function calculateWinner(userMove, aiMove) {
-  return '???'
+  if (userMove === "rock" && aiMove === "paper") {
+    return "Computer";
+  }
+  else if (userMove === "rock" && aiMove === "scissors") {
+    return "You";
+  }
+  else if (userMove === "rock" && aiMove === "rock") {
+    return "Draw";
+  } 
+  else if (userMove === "paper" && aiMove === "scissors") {
+    return "Computer";
+  }
+  else if (userMove === "paper" && aiMove === "rock") {
+    return "You";
+  }
+  else if (userMove === "paper" && aiMove === "paper") {
+    return "Draw";
+  }
+  else if (userMove === "scissors" && aiMove === "rock") {
+    return "Computer";
+  }
+  else if (userMove === "scissors" && aiMove === "paper") {
+    return "You";
+  }
+  else if (userMove === "scissors" && aiMove === "scissors") {
+    return "Draw";
+  }
+  else {
+    return "None!!! Invalid input! Please try again!";
+  }
 }
 
 module.exports = {
